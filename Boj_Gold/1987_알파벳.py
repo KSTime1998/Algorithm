eb45ef73d4a54036b96490ac0d67bc5a0fc517af
KSTime1998 <90090509+KSTime1998@users.noreply.github.com>
2022-01-15 13:08:40 +0900

    # 1987 - 알파벳

--- Boj_Gold/1987_알파벳.py
+++ Boj_Gold/1987_알파벳.py
@@ -3,23 +3,24 @@ import sys
 R,C = map(int,sys.stdin.readline().split())
 board = [list(sys.stdin.readline().rstrip()) for _ in range(R)]
 ans = 0
-used_alphabet = set()
 
-dx = [-1,0,0,1]
-dy = [0,-1,1,0]
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
 
-def dfs(x,y,val):
+def bfs():
   global ans
-  ans = max(ans, val)
-  for d in range(4):
-    X,Y = x+dx[d],y+dy[d]
-    if 0 <= X < R and 0 <= Y < C:
-      if board[X][Y] in used_alphabet:
-        continue
-      used_alphabet.add(board[X][Y])
-      dfs(x+dx[d],y+dy[d],val+1)
-      used_alphabet.remove(board[X][Y])
+  alpha = set()
+  alpha.add((0,0,board[0][0]))
 
-used_alphabet.add(board[0][0])
-dfs(0,0,1)
+  while alpha:
+    x,y,al = alpha.pop()
+    ans = max(ans, len(al))
+    for d in range(4):
+      X,Y = x+dx[d],y+dy[d]
+      if 0 <= X < R and 0 <= Y < C:
+        if board[X][Y] in al:
+          continue
+        alpha.add((X,Y,al+board[X][Y]))
+
+bfs()
 print(ans)
